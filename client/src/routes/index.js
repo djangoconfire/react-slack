339b7b9b3f5e53be6175d09ec0771608afb904d0
djangoconfire <djangopycon@gmail.com>
2019-05-13 11:56:55 +0530

    new route for login

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+// Components
 import Home from './Home';
 import Register from './Register';
+import LoginComponent from './login';
+
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
+      <Route path="/login" exact component={LoginComponent} />
     </Switch>
   </BrowserRouter>
 );
