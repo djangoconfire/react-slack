4701431958e2758af15f7c501faf899b30ce2c7e
djangoconfire <djangopycon@gmail.com>
2019-05-14 14:19:51 +0530

    implemented private route

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch , Redirect } from 'react-router-dom';
 
 // Components
 import Home from './Home';
 import Register from './Register';
 import LoginComponent from './login';
 import CreateTeamComponent from './CreateTeam';
+import decode from 'jwt-decode';
+
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  try {
+    decode(token);
+    decode(refreshToken);
+  } catch (err) {
+    return false;
+  }
+
+  return true;
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      (isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/login',
+          }}
+        />
+      ))}
+  />
+);
 
 export default () => (
   <BrowserRouter>
@@ -13,7 +43,7 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={LoginComponent} />
-      <Route path="/create_team" exact component={CreateTeamComponent} />
+      <PrivateRoute path="/create_team" exact component={CreateTeamComponent} />
     </Switch>
   </BrowserRouter>
 );
