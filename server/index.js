d500c777860517d2849ee65de283248200b4b878
djangoconfire <djangopycon@gmail.com>
2019-05-14 13:25:48 +0530

    added a miidle ware - addUSer

--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ import { makeExecutableSchema } from 'graphql-tools';
 import path from 'path';
 import cors from 'cors';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
-
+import jwt from 'jsonwebtoken';
 import models from './models';
+import { refreshTokens } from './auth';
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
@@ -20,23 +21,46 @@ const schema = makeExecutableSchema({
   resolvers,
 });
 
-const app = express();
+const app = express(); 
 app.use(cors('*'));
+
+const addUser = async (req, res, next) => { 
+  const token = req.headers['x-token'];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (err) {
+      const refreshToken = req.headers['x-refresh-token'];
+      const newToken = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+      if (newToken.token && newToken.refreshToken) {
+        res.set('Access-Control-Expose-Headers', 'x-token', 'x-refresh-token');
+        res.set('x-token', newToken.token);
+        res.set('x-refresh-token', newToken.refreshToken);
+      }
+      req.user = newToken.user;
+    }
+  }
+  next();
+};
+
+app.use(addUser);
 
 const graphqlEndpoint = '/graphql';
 
 app.use(
   graphqlEndpoint, bodyParser.json(), 
-  graphqlExpress({ 
+  graphqlExpress(req => ({ 
     schema, 
     context: { 
       models, 
-      user: { id: 1 }, 
+      user: req.user,
       SECRET,
       SECRET2,
-      } 
-    })  
-  );
+    }
+  }),
+  ),
+);
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
--- server/resolvers/team.js
+++ server/resolvers/team.js
@@ -1,22 +1,21 @@
-// import { formatErrors } from '../formatError';
+// import formatErrors from '../formatErrors';
+import requiresAuth from '../permissions';
 
 export default {
-    Query: {
-        allTeams: (parent, args, { models }) => models.Team.findAll(),
-    },
-    Mutation: {
-        createTeam: async (parent, args, { models, user }) => {
-            try {
-                await models.Team.create({ ...args, owner: user.id });
-                return {
-                    ok: true,
-                }
-            } catch(err) {
-                return {
-                    ok: false,
-                    errors: [{ path: 'name', message: 'Something went wrong!'}],
-                }
-            }
-        }
-    }
+  Mutation: {
+    createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
+      try {
+        await models.Team.create({ ...args, owner: user.id });
+        return {
+          ok: true,
+        };
+      } catch (err) {
+        console.log(err);
+        return {
+          ok: false,
+          errors: [{ path: 'name', message: 'Something went wrong!'}],
+        };
+      }
+    }),
+  },
 };
